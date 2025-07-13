ab5f45d1e366638f4c79abc1a01e1d36a439d4ca
Praveen Aththanayake <aththanayakempa@gmail.com>
2025-07-13 11:26:28 +0530

    initial commit

---
+++ Bodima/BodimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodimaApp.swift
+//  Bodima
+//
+//  Created by Praveen Aththanayake on 2025-07-13.
+//
+
+import SwiftUI
+
+@main
+struct BodimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodimaTests/BodimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  BodimaTests.swift
+//  BodimaTests
+//
+//  Created by Praveen Aththanayake on 2025-07-13.
+//
+
+import Testing
+@testable import Bodima
+
+struct BodimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
